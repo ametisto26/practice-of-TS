0c5158ca78eae9e894bf811205c94b0c06013760
J. Ametisto <139443058+ametisto26@users.noreply.github.com>
2023-07-17 00:45:01 +0900

    create hello.ts and .gitignore

---
+++ hello.js
@@ -0,0 +1,9 @@
+console.log("Hello World!");
+var str = "Hello!";
+console.log(str);
+/*
+console.log("Hello World!");
+console.log("Hello World!");
+console.log("Hello World!");
+*/
+// tab + / 
---
+++ hello.ts
@@ -0,0 +1,12 @@
+console.log("Hello World!");
+
+const str: string = "Hello!";
+console.log(str);
+
+/*
+console.log("Hello World!");
+console.log("Hello World!");
+console.log("Hello World!");
+*/
+
+// tab + / 
