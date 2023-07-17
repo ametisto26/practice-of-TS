c42f2d62e5b2f8ecba4905cd4f8a228dccc7a59a
J. Ametisto <139443058+ametisto26@users.noreply.github.com>
2023-07-18 01:51:32 +0900

    update exercise.ts

--- dist/exercise.js
+++ dist/exercise.js
@@ -27,4 +27,40 @@ const n = 6;
 if (n % 2 === 0 && n % 3 === 0) {
     console.log(`${n}は2の倍数かつ3の倍数である。`);
 }
+//switch の練習
+let rank = Math.floor(Math.random() * 6) + 1;
+switch (rank) {
+    case 1:
+        console.log("Gold");
+        break;
+    case 2:
+        console.log("Silver");
+        break;
+    case 3:
+        console.log("Bronze");
+        break;
+    case 4:
+        console.log("4th");
+        break;
+    case 5:
+        console.log("5th");
+        break;
+    default:
+        console.log("Other");
+        break; //最後のbreakは書かなくてもあまり問題ない。
+}
+let x1;
+console.log(x1); //undefined
+let x2 = null;
+console.log(x2);
+if (x1 === undefined) {
+    console.log("undefined!");
+}
+if (x2 === null) {
+    console.log("null!");
+}
+//三項演算子
+const age = 20;
+const drink = (age >= 20) ? "Beer" : "Cola";
+console.log(drink);
 //# sourceMappingURL=exercise.js.map
